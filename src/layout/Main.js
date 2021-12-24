6509481d79d8c8bdaea44524cad1cff81e3bf622
Krigston <sedovwwwwb.bbk.r1235@gmail.com>
2021-12-24 13:36:53 +0700

    upload

---
+++ src/layout/Main.js
@@ -0,0 +1,80 @@
+import React from 'react';
+import Movies from '../components/Movies';
+import Search from '../components/Search';
+import Movie from '../components/Movie';
+
+class Main extends React.Component {
+    state = {
+        show: 'index',
+        movies: [],
+        movie: {},
+        loading: true,
+    }
+
+    handleEnter = (search, type) => {
+        if (search.trim() === "") return;
+        this.setState({
+            loading: true,
+        show: 'search'
+        });
+        search = encodeURIComponent(search);
+        let url = `https://www.omdbapi.com/?i=tt3896198&apikey=c8ceb044&s=${search}`;
+        if (type !== 'all') {
+            url = url + `&type=${type}`;
+        }
+        fetch(url)
+            .then(response => response.json())
+            .then(data => {
+                this.setState({
+                    movies: data.Search ? data.Search : [],
+                    loading: false
+                });
+            });
+    }
+
+    handleReadMore = (id) => {
+        this.setState({
+            loading: true,
+            show: 'movie'
+        });
+        fetch(`https://www.omdbapi.com/?i=tt3896198&apikey=c8ceb044&s=${id}&plot=full`)
+            .then(response => response.json())
+            .then(data => {
+                this.setState({
+                    movie: data.Title ? data : {},
+                    loading: false
+                });
+            });
+    }
+
+    componentDidMount() {
+        fetch('https://www.omdbapi.com/?i=tt3896198&apikey=c8ceb044&s=matrix')
+            .then(response => response.json())
+            .then(data => {
+                this.setState({
+                    movies: data.Search ? data.Search : [],
+                    loading: false
+                });
+            });
+    }
+
+    render() {
+        return (
+            <main className="container">
+                <Search enterHandler={this.handleEnter} />
+                {this.state.loading ? (
+                    <div className="loader">Загрузка...</div>
+                ) : this.state.show === 'movie' ? (
+                    <Movie {...this.state.movie} />
+                ) : (
+                    <Movies
+                        movies={this.state.movies}
+                        readMoreHandler={this.handleReadMore}
+                    />
+                )}
+            </main>
+        );
+    }
+}
+
+export default Main;
